3fc5569649b34db38465253e4c46aa928af6efbe
Lewis Nozaki <lhnozaki@gmail.com>
2019-07-11 00:12:16 -1000

    passed tests

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,14 @@
-function chirp(n){
+function chirp(n) {
   // FIX ME
+  let bird = "chirp ";
+
+  if (n === 1) {
+    return bird.substr(0, bird.length - 1);
+  }
+
+  return bird + chirp(n - 1);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("#result").html(chirp(3));
 });
