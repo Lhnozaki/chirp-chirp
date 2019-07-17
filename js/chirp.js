798e5e2e273c49e39dc07c1b54fca84265369a5d
Lewis Nozaki <lhnozaki@gmail.com>
2019-07-16 20:42:04 -1000

    fixed code

--- js/chirp.js
+++ js/chirp.js
@@ -1,12 +1,10 @@
 function chirp(n) {
   // FIX ME
-  let bird = "chirp ";
-
   if (n === 1) {
-    return bird.substr(0, bird.length - 1);
+    return "chirp";
   }
 
-  return bird + chirp(n - 1);
+  return "chirp " + chirp(n - 1);
 }
 
 $(document).ready(function() {
